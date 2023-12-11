6241bcb36952319da3a9b97b542290763d50aa5c
EC2 Default User <ec2-user@ip-172-31-24-132.ap-northeast-1.compute.internal>
2023-12-11 12:40:31 +0000

    users showページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book_new = Book.new
-    @book = Book.find(params[:id])
+    @book = @user.books
   end
 
   def edit
